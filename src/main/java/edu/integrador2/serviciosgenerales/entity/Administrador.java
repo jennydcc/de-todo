480870b6dfaea6de5842964b443dedc7b0fb9eea
Jaime Cruz <jaimecruz.inbox@gmail.com>
2021-06-03 00:02:24 -0400

    Navegaciòn

---
+++ src/main/java/edu/integrador2/serviciosgenerales/entity/Administrador.java
@@ -0,0 +1,25 @@
+package edu.integrador2.serviciosgenerales.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Especialista implementa UserDetails para el proceso de autenticación
+ */
+@Entity
+@Getter
+@Setter
+@Table(name = "especialista2")
+public class Especialista extends Usuario {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id_especialista", unique = true, nullable = false)
+  private Long id;
+}
