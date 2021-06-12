27c5b116b6e84d8e9430df06b4e90722d78efeb7
igomez4405 <61365387+igomez4405@users.noreply.github.com>
2021-06-12 17:11:38 -0500

    Formulario de actualizar cliente y especialista

--- src/main/java/edu/integrador2/serviciosgenerales/controller/ClienteController.java
+++ src/main/java/edu/integrador2/serviciosgenerales/controller/ClienteController.java
@@ -23,13 +23,13 @@ public class ClienteController {
   private ModelMapper modelMapper;
   @Autowired
   ClienteService entityService;
-
   @Autowired
   EspecialidadService especialidadService;
   @Autowired
   ServicioService servicioService;
   @Autowired
   EspecialistaService especialistaService;
+
 
   @GetMapping("/cliente/")
   public String homePage(Model uiModel) {
@@ -63,13 +63,12 @@ public class ClienteController {
     uiModel.addAttribute("serviciosSolicitados", servicioService.listar());
     return "cliente/servicios-solicitados";
   }
+
+
+
 /**
  * Modificar datos del cliente 
  * */
-
-
-
-
 
 
   /**
@@ -85,6 +84,7 @@ public class ClienteController {
     return "cliente/solicitar-servicio";
   }
 
+
   @PostMapping(path = "/cliente/solicitar-servicio", consumes = { MediaType.APPLICATION_FORM_URLENCODED_VALUE })
   public String registrarEspecialista(ServicioDto dto, Model uiModel) {
     Servicio entity = modelMapper.map(dto, Servicio.class);
@@ -99,4 +99,15 @@ public class ClienteController {
     Template.addPageIndex(uiModel, 2);
     return "cliente/reportes";
   }
+
+  @GetMapping("/cliente/modificar")
+  public String modificarclientes(Model uiModel) throws Exception {
+    Template.addGlobalAttributes(uiModel);
+    Template.addPageIndex(uiModel, 3);
+    return "cliente/modificar";
+  }
+  
+
+
+
 }
